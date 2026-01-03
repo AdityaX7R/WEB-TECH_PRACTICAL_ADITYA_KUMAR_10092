b36981b44143f29101b2529958e413a1fa07afad
Aditya Kumar <adikr4477@gmail.com>
2026-01-03 19:33:00 +0000

    hide HTML elements using JavaScript and toggle CSS classes dynamically

---
+++ JS_EXPERIMENT/exp_no_13.js
@@ -0,0 +1,11 @@
+// Show or Hide element
+function toggleBox() {
+    var box = document.getElementById("box");
+    box.classList.toggle("hidden");
+}
+
+// Toggle CSS class dynamically
+function toggleTheme() {
+    var box = document.getElementById("box");
+    box.classList.toggle("highlight");
+}
